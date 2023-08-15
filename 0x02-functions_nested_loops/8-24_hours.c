5fe25c3334653f5ce5542e3f25da700665f10b5c
Dayebo Gurbe <gdayeboet@.com>
2023-08-15 07:50:46 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,31 +2,26 @@
 
 /**
  * jack_baur - Prints every minute of the day of Jack Baur
- * return: 1 always
+ * Description: Print the time in minutes for 24 hrs
+ *
+ * Return: time in minutes
  */
-void jack_baur(void)
+void jack_bauer(void)
 {
-	int a, b, c, d;
 
-	for (a = 0; a <= 2; a++)
+	int H, M;
+
+	for (H = 0; H < 24; H++)
 	{
-		for (b = 0, b <= 9; b++)
+		for (M = 0; M < 60; M++)
 		{
-			if ((a <= 1 && <= 9) || (a <= 2 && b <= 3)
-					{
-					for (c = 0; c <= 5; c++)
-					{
-					for (d = 0, d <= 0; d++)
-					{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(58);
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-					}
-					}
-					}
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+
 		}
 	}
 }
