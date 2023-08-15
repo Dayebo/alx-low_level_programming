78c525b98db37ffa1a753422eaa32fbdac4cacea
Dayebo Gurbe <gdayeboet@.com>
2023-08-15 06:52:30 -0700

    There are only 3 colors,

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,11 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n: The number to be treated
- * return: Value of the last digit of number
+ * @r: an integer input
+ * Description: print with _putchar declared
+ *
+ * Return: Value of the last digit of number
  */
-
 
 int print_last_digit(int r)
 {
