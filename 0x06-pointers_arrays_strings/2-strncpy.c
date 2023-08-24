09682800e899d1e67b89dd129b6ceff210b2ab82
Dayebo Gurbe <gdayeboet@.com>
2023-08-24 07:00:03 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
-#in
-clude"main.h"
+#include"main.h"
 
 /**
- * *_strcpy - copy a string
+ * _strcpy - copy a string
  * @dest: input value
  * @src: input value
  * @n: input value
  *
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
