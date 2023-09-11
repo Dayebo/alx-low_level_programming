4dd0a9eeb4e908e1593e7db27bf50f4b364ba12f
Dayebo Gurbe <gdayeboet@.com>
2023-09-11 09:58:18 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,14 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * init_dog - initialize a variable of type struct dog
- * Struct_dog – a dog’s basic info
+ * struct dog – a dog’s basic info
+ *
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
+ * Description: first struct with alx
  */
 
 struct dog
@@ -15,13 +17,5 @@ struct dog
 	float age;
 	char *owner;
 };
-dog_t *new_dog(char *name, float age, char *owner);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-typedef struct dog dog_t;
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
