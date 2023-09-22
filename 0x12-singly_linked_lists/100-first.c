5a2f49cd2db9e1b5c5d5032d587fbb5c048c3d84
Dayebo <gdayeboet@gmail.com>
2023-09-22 00:26:49 -0700

    6. Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void first(void) __attribute__ ((nonstructor));
+
+/**
+ * first - prints a sentence befor the main
+ * function is excuted
+ */
+
+void first(void)
+{
+printf("You're best! amd yet, you must allow.\n");
+printf("I bore my house upon my back!\n");
+}
