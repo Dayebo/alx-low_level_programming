60da3d3d7693262e7a7c43d8c5dbe8fccc34ddf6
Dayebo <gdayeboet@gmail.com>
2023-09-22 01:09:27 -0700

    tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
-#include"lists.h"
+#include<stdio.h>
 
-void ComeFirstbyCTC(void) __attributr__ ((construc))
+void first(void) __attribute__ ((constructor));
 
 /**
- * ComeFirst - Functions that run befor the main function
+ * first - prints a sentence before the main function is executed
  */
 
-void ComeFirstbyCTC(void)
+void first(void)
 {
-	printf(You're best! amd yet, you must allow,\n);
-	printf(I bore my house upon my back!\n);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
