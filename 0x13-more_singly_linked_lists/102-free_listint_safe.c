0d5751c167282d423bbcdfc1679bb11d04d8217d
Dayebo <gdayeboet@gmail.com>
2023-09-26 02:37:16 -0700

    13. Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,9 +3,9 @@
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first nodes
+ *
  * Return: number of elements
  */
-
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
